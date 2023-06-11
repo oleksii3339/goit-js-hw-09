76c2fe2d5b1de912fda8810eeafb6530ae9a34c9
oleksii3339 <122258262+oleksii3339@users.noreply.github.com>
2023-06-11 15:19:26 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const bodyEl = document.body;
+const btnStartEl = document.querySelector([data-start]);
+const btnStopEl = document.querySelector([data-stop]);
 
+let interval = null;
+
+function colorChange() {
+    interval = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStartEl.disabled = true;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
